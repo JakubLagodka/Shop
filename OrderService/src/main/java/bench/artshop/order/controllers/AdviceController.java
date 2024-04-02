18f42108947e67ccd77d483bc928b491acd6cbbc
jbad <jbad@gft.com>
2024-04-02 12:52:17 +0200

    fixed lombok log

--- OrderService/src/main/java/bench/artshop/order/controllers/AdviceController.java
+++ OrderService/src/main/java/bench/artshop/order/controllers/AdviceController.java
@@ -1,21 +1,22 @@
 package bench.artshop.order.controllers;
 
 import jakarta.persistence.EntityNotFoundException;
+import lombok.extern.java.Log;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.logging.Level;
-import java.util.logging.Logger;
+
+
 
 @ControllerAdvice
+@Log
 public class AdviceController {
-    private Logger logger;
     @ExceptionHandler(EntityNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public void handleEntityNotFoundException(EntityNotFoundException e) {
-        logger.log(Level.WARNING,"Order by given id not found!");
-
+        log.log(Level.WARNING,"Order by given id not found!");
     }
 }
