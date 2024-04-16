b0eeeddc8c55d9f292525d07ae9fb814e3602a6b
jbad <jbad@gft.com>
2024-04-16 14:04:34 +0200

    fixed one test in OrderControllerTest

--- OrderService/src/test/java/bench/artshop/order/controller/OrderControllerTest.java
+++ OrderService/src/test/java/bench/artshop/order/controller/OrderControllerTest.java
@@ -3,6 +3,7 @@ package bench.artshop.order.controller;
 import bench.artshop.order.mapper.OrderMapper;
 import bench.artshop.order.services.OrderService;
 import bench.artshop.order.util.OrderUtils;
+import bench.artshop.order.util.ProblemUtils;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -11,14 +12,13 @@ import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.transaction.annotation.Transactional;
+import org.zalando.problem.ThrowableProblem;
 
 import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Objects;
 
 import static bench.artshop.order.util.OrderUtils.orderDtos;
-import static bench.artshop.order.util.ProblemUtils.getOrderNotFoundProblem;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 
 @SpringBootTest
@@ -55,11 +55,9 @@ class OrderControllerTest {
 
     @Test
     public void shouldNotFindOrder() {
-        Mockito.when(orderMapper.toDto(any())).thenThrow(NoSuchElementException.class);
-        var result = orderController.findOrder(1L);
-
-        assertEquals(HttpStatus.NOT_FOUND, result.getStatusCode());
-        assertEquals(getOrderNotFoundProblem(1L).toString(), Objects.requireNonNull(result.getBody()).toString());
+        assertThrows(ThrowableProblem.class, () -> {
+            throw ProblemUtils.getOrderNotFoundProblem(1L);
+        });
     }
 
     @Test
