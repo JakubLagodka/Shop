004674e45e1fa3740253cac51a9337f304d211ca
jbad <jbad@gft.com>
2024-05-10 13:43:35 +0200

    finally found typo issue and repaired, added SecurityConfiguration

--- OrderService/src/main/java/bench/artshop/order/config/SecurityConfiguration.java
+++ OrderService/src/main/java/bench/artshop/order/config/SecurityConfiguration.java
@@ -2,18 +2,18 @@ package bench.artshop.order.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
 
-@EnableWebSecurity
+import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
-//    @Bean
+    //    @Bean
 //    public UserDetailsService userDetailsService() {
 //        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
 //        manager.createUser(User.withUsername("user")
@@ -26,11 +26,28 @@ public class SecurityConfiguration {
 //                .build());
 //        return manager;
 //    }
+     public static final String[] AUTH_WHITELIST = {
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**",
+            "/v3/api-docs/**",
+            "/api/public/**",
+            "/api/public/authenticate",
+            "/actuator/*",
+            "/swagger-ui/**",
+            "/api/login/**",
+            "/api/order/**",
+            "/api/user/**"
+    };
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 //        http.csrf(AbstractHttpConfigurer::disable)
@@ -44,20 +61,21 @@ public class SecurityConfiguration {
 //                .sessionManagement(httpSecuritySessionManagementConfigurer ->
 //                        httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http
-                .csrf(Customizer.withDefaults())
-                .authorizeHttpRequests(authorize -> authorize
-                                .requestMatchers(HttpMethod.POST).permitAll()
-                                .requestMatchers("/login/**").permitAll()
-                                .requestMatchers("/order/**").permitAll()
-                                .requestMatchers("/user/**").permitAll()
-//                        .anyRequest().authenticated()
-                )
-                .httpBasic(Customizer.withDefaults())
-                .formLogin(Customizer.withDefaults());
+//                .csrf(Customizer.withDefaults())
+//                .authorizeHttpRequests(authorize -> authorize
+////                                .requestMatchers(HttpMethod.POST).permitAll()
+//                                .requestMatchers(AUTH_WHITELIST).permitAll()
+//                                .anyRequest().denyAll()
+////                                .anyRequest().authenticated()
+//                )
+
+//                .formLogin(Customizer.withDefaults());
+                .addFilterBefore(new SecurityFilter(), AuthorizationFilter.class)
+                .httpBasic(withDefaults());
         return http.build();
     }
 //    @Bean
 //    public WebSecurityCustomizer webSecurityCustomizer() {
-//        return web -> web.debug(securityDebug).ignoring().requestMatchers("/css/**", "/js/**", "/img/**", "/lib/**", "/favicon.ico");
+//        return web -> web.ignoring().requestMatchers(AUTH_WHITELIST);
 //    }
 }
--- OrderService/src/main/java/bench/artshop/order/controller/UserController.java
+++ OrderService/src/main/java/bench/artshop/order/controller/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/api/user")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
